21866b27079bb1adb5fd3e7b42c21fdb8422d894
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-13 16:57:26 +0530

    Practiced variables and constants and how to get the type and the memory address of the variables

---
+++ variables_demo.rb
@@ -0,0 +1,16 @@
+a = 10
+b = 23
+str = "Hi I am string in Ruby"
+# Constant
+A = 45
+# A = 23 gives an error
+
+puts a.object_id
+puts b.object_id
+puts str.object_id
+puts A.object_id
+
+puts a.class
+puts b.class
+puts str.class
+puts A.class
