1b2427ca76f46df1e85e1545a094b1fe0fe0b6ce
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-13 23:16:43 +0530

    Practiced local variables

---
+++ types_of_variables.rb
@@ -0,0 +1,13 @@
+#Ruby has 5 types of variables
+#1. Local Variables -> Always starts with a lowercase letter or an underscore && The scope is block level
+a = 10
+b = 12
+
+if a < b
+    puts "a is less than b"
+else
+    puts "a is greater than b"
+#2. Class Variables -> 
+#3. Instance Variables -> 
+#4. Global Variables -> 
+#5. Constants
