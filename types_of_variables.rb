208d86d5cc056302780d9c8115f4187ecf71a7a5
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-13 23:22:20 +0530

    Practiced class variables

--- types_of_variables.rb
+++ types_of_variables.rb
@@ -7,7 +7,15 @@ if a < b
     puts "a is less than b"
 else
     puts "a is greater than b"
-#2. Class Variables -> 
+#2. Class Variables -> Always starts with double '@' sign && Class variable must be used in the class only
+class Student
+    @@student_count = 0
+
+    def total_students
+        @@student_count += 1
+        puts "Total students are #{@@student_count}"
+    end
+end
 #3. Instance Variables -> 
 #4. Global Variables -> 
 #5. Constants
