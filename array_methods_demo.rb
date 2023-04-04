da8c1758a22a2e0f8f8ddee45e820f53ca1b7b14
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-04 10:18:57 +0530

    Practiced array methods in Ruby

---
+++ array_methods_demo.rb
@@ -0,0 +1,20 @@
+array = [1,2,3,4]
+
+puts array.sort
+puts array.reverse
+puts array
+puts array.push(7)
+puts array.unshift(9)
+puts array.shift
+puts array.pop
+puts array.delete(3)
+puts array.at(2)
+puts array.include? 2
+# puts array.methods
+puts array.min
+puts array.max
+puts array.uniq
+puts array.product
+puts array.flatten
+array << nil
+puts array.compact.size
