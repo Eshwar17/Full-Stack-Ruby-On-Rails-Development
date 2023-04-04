68c5f478c8260e574a44f1b3e85a979cca32e9fa
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-04 09:50:41 +0530

    Practiced array creations in Ruby

---
+++ array_demo.rb
@@ -0,0 +1,12 @@
+array = Array.new(10, 'Eshu')
+# array = [1, 2]
+# array = Array.new()
+# array = Array.new(size, ele)
+array << 1
+array << 2
+array << nil
+array << true
+array << [1,2,3]
+
+puts array
+puts array.size
